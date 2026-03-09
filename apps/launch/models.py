5618ab56ef5a393c6dc274a7b0084b7eaa42f52d
ethanresek <resekethan@gmail.com>
2026-03-09 19:57:46 -0400

    Complete initial LaunchApplication class

--- apps/launch/models.py
+++ apps/launch/models.py
@@ -34,10 +34,74 @@ class LaunchProject(TimestampMixin):
 
     class Meta:
         db_table = "launch_projects"
-        ordering = ["-submitted_at"]
+        ordering = ["-created_at"]
         verbose_name = "Launch Project"
         verbose_name_plural = "Launch Projects"
 
     def __str__(self):
         return f"Launch Project: {self.title} — {self.team.first_name} {self.team.last_name} ({self.team.email})"
 
+class LaunchApplication(TimestampMixin):
+    
+    # Foreign keys
+    user = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.CASCADE,
+        related_name="launch_applications"
+    )
+    project = models.ForeignKey(
+        LaunchProject,
+        on_delete=models.CASCADE,
+        related_name="launch_applications"
+    )
+    cycle = models.ForeignKey(
+        "cycles.ApplicationCycle",
+        on_delete=models.CASCADE,
+        related_name="launch_applications"
+    )
+
+    # Non-key attributes
+    resume = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True,
+        help_text="Link to applicant's resume"
+    )
+    portfolio = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True,
+        help_text="Link to applicant's portfolio"
+    )
+
+    responses = models.JSONField(
+        null=True,
+        blank=True,
+        help_text="Responses to application"
+    )
+
+    class Status(models.TextChoices):
+        SUBMITTED = "SUBMITTED", "Submitted"
+        FILTERED = "FILTERED", "Filtered"
+        SENT_TO_TEAM = "SENT_TO_TEAM", "Sent to team"
+        SELECTED = "SELECTED", "Selected"
+        NOT_SELECTED = "NOT_SELECTED", "Not selected"
+        WITHDRAWN = "WITHDRAWN", "Withdrawn"
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.SUBMITTED
+    )
+
+    class Meta:
+        db_table = "launch_applications"
+        ordering = ["-created_at"]
+        verbose_name = "Launch Application"
+        verbose_name_plural = "Launch Applications"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "project", "cycle"],
+                name="unique_la_per_user_per_project_per_cycle"
+            )
+        ]
