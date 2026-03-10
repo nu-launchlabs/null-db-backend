6e69ab8d9689aaeb9488888d33e9752e7de3b405
ethanresek <resekethan@gmail.com>
2026-03-09 20:48:06 -0400

    Complete initial LaunchCandidate class

--- apps/launch/models.py
+++ apps/launch/models.py
@@ -1,7 +1,23 @@
+"""
+Models related to NULL Launch Projects. Models included:
+    LaunchProject
+    LaunchApplication
+    LaunchCandidate
+"""
+
 from django.db import models
 from utils.mixins import TimestampMixin
 
 class LaunchProject(TimestampMixin):
+    """
+    Model for storing information about Launch Team Projects.
+    """
+
+    class Meta:
+        db_table = "launch_projects"
+        ordering = ["-created_at"]
+        verbose_name = "Launch Project"
+        verbose_name_plural = "Launch Projects"
 
     # Foreign keys
     cycle = models.ForeignKey(
@@ -32,16 +48,34 @@ class LaunchProject(TimestampMixin):
         default=4
     )
 
-    class Meta:
-        db_table = "launch_projects"
-        ordering = ["-created_at"]
-        verbose_name = "Launch Project"
-        verbose_name_plural = "Launch Projects"
-
     def __str__(self):
         return f"Launch Project: {self.title} — {self.team.first_name} {self.team.last_name} ({self.team.email})"
 
 class LaunchApplication(TimestampMixin):
+    """
+    Model for storing information regarding launch project applications.
+    """
+
+    class Meta:
+        db_table = "launch_applications"
+        ordering = ["-created_at"]
+        verbose_name = "Launch Application"
+        verbose_name_plural = "Launch Applications"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "project", "cycle"],
+                name="unique_la_per_user_per_project_per_cycle"
+            )
+        ]
+
+    # Status class used to limit domain in status field
+    class Status(models.TextChoices):  
+        SUBMITTED = "SUBMITTED", "Submitted"
+        FILTERED = "FILTERED", "Filtered"
+        SENT_TO_TEAM = "SENT_TO_TEAM", "Sent to team"
+        SELECTED = "SELECTED", "Selected"
+        NOT_SELECTED = "NOT_SELECTED", "Not selected"
+        WITHDRAWN = "WITHDRAWN", "Withdrawn"
     
     # Foreign keys
     user = models.ForeignKey(
@@ -73,35 +107,68 @@ class LaunchApplication(TimestampMixin):
         blank=True,
         help_text="Link to applicant's portfolio"
     )
-
     responses = models.JSONField(
         null=True,
         blank=True,
         help_text="Responses to application"
     )
-
-    class Status(models.TextChoices):
-        SUBMITTED = "SUBMITTED", "Submitted"
-        FILTERED = "FILTERED", "Filtered"
-        SENT_TO_TEAM = "SENT_TO_TEAM", "Sent to team"
-        SELECTED = "SELECTED", "Selected"
-        NOT_SELECTED = "NOT_SELECTED", "Not selected"
-        WITHDRAWN = "WITHDRAWN", "Withdrawn"
-
     status = models.CharField(
         max_length=20,
         choices=Status.choices,
-        default=Status.SUBMITTED
+        default=Status.SUBMITTED,
+        help_text="The status of the application, e.g., Submitted, Selected, etc"
     )
+    
+    def __str__(self):
+        return f"Launch Application: {self.user.first_name} {self.user.last_name} — {self.project.title} {self.cycle.name}"
+
+class LaunchCandidate(TimestampMixin):
+    """
+    Model for storing information about applicants under consideration for launch projects.
+    """
 
     class Meta:
-        db_table = "launch_applications"
+        db_table = "launch_candidates"
         ordering = ["-created_at"]
-        verbose_name = "Launch Application"
-        verbose_name_plural = "Launch Applications"
+        verbose_name = "Launch Candidate"
+        verbose_name_plural = "Launch Candidates"
         constraints = [
             models.UniqueConstraint(
-                fields=["user", "project", "cycle"],
-                name="unique_la_per_user_per_project_per_cycle"
+                fields=["application", "project"],
+                name="unique_lc_per_application_per_project"
             )
         ]
+    
+    # Status class used to limit domain in status field
+    class Status(models.TextChoices):
+        PENDING_REVIEW = "PENDING_REVIEW", "Pending review"
+        SELECTED = "SELECTED", "Selected"
+        REJECTED = "REJECTED", "Rejected"
+    
+    # Foreign keys
+    application = models.ForeignKey(
+        LaunchApplication,
+        on_delete=models.CASCADE,
+        related_name="launch_candidates"
+    )
+    project = models.ForeignKey(
+        LaunchProject,
+        on_delete=models.CASCADE,
+        related_name="launch_candidates"
+    )
+
+    # Non-key attributes
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING_REVIEW,
+        help_text="The status of the candidate, e.g., Selected, Rejected, etc"
+    )
+    selected_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text="The DateTime a candidate is selected for a given project"
+    )
+
+    def __str__(self):
+        return f"Launch Candidate: {self.application.user.first_name} {self.application.user.last_name} — {self.project.title} {self.application.cycle.name}"
